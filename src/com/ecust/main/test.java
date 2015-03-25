88ac9ad92bf5a8085510065c1f2285a3564ae771
Rzehpk <gaoty@qq.com>
2015-03-25 16:42:16 +0800

    修改test.java,测试DBOperation.java

--- src/com/ecust/main/test.java
+++ src/com/ecust/main/test.java
@@ -3,7 +3,7 @@
  */
 package com.ecust.main;
 
-
+import com.ecust.database.DBOperation;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import com.ecust.orm.SpiderModle;
@@ -23,14 +23,9 @@ public class test {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		ArrayList<String> a = new ArrayList<String>() ;
-		a.add("a1");
-		a.add("a2");
-		a.add("a3");
-		SpiderModle item = new SpiderModle("1","2","3","4",a,"5","6","7"); 
+		String sql= "SELECT * FROM  `jditems` LIMIT 0 , 30";
 		try{
-			item.insert();
-			System.out.println("ok");
+			System.out.println(DBOperation.execSQL(sql));
 		}catch(SQLException e){
 			System.out.println(e);
 		}
